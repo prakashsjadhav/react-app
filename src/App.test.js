22ead5ea3ed310ded6b00d68a6235c3d924a18a0
Prakash Jadhav <prakashsjadhav2012@gmail.com>
2022-06-10 18:38:33 +0530

    Few more Changes in app.js and app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react hands-on/i);
+  const linkElement = getByText(/learn react hands-on effectively/i);
   expect(linkElement).toBeInTheDocument();
 });
