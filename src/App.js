7b18956a7e1b0356aefd0f8d89541d0a4f8ce7d5
Prakash Jadhav <prakashsjadhav2012@gmail.com>
2022-06-10 21:47:53 +0530

    Removed package-lock.json

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Hands-on effectively
+          Learn React Hands-on in a different way
         </a>
       </header>
     </div>
